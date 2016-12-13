e648472374a9738b5d45dc70aed1d981038642b0
Sam Lakerveld <darkwater124@gmail.com>
2016-12-13 16:19:44 +0100

    Add functionality to add/remove bookmarks

--- src/main.d
+++ src/main.d
@@ -1,4 +1,4 @@
-module priv_cmd;
+module priv.cmd;
 
 import std.algorithm;
 import std.conv;
@@ -7,65 +7,80 @@ import std.format;
 import std.getopt;
 import std.json;
 import std.process;
-import std.regex;
+import std.random;
 import std.range;
+import std.regex;
 import std.stdio;
 import std.string;
 
 import tinyredis;
 
-struct ScreentoolOptions
-{
-    bool foo;
-    bool bar;
+struct PrivCmdOptions {
+    string add;
+    string remove;
 
-    bool validate(ref string message)
-    {
+    bool validate(ref string message) {
         message = "ye is gud";
         return true;
     }
 }
 
-ScreentoolOptions options;
+GetoptResult helpInfo;
+PrivCmdOptions options;
 
-int main(string[] args)
-{
-    auto helpInfo = getopt(args,
-            std.getopt.config.caseSensitive,
-            "f|foo", "Use a shortened URL where availible", &options.foo,
-            "b|bar", "Don't send notifications",            &options.bar);
+string username;
+string envName;
 
-    if (helpInfo.helpWanted)
-    {
-        writeln("priv-cmd v0.1.0");
-        writeln("");
-        writeln("Options:");
+Redis redisMaster;
+Redis redisLocal;
 
-        ulong longestShortOptLength = helpInfo.options.map!( opt => opt.optShort.length )
-            .reduce!((a, b) => max(a, b));
-        ulong longestLongOptLength  = helpInfo.options.map!( opt => opt.optLong.length  )
-            .reduce!((a, b) => max(a, b));
+void usage() {
+    writeln("priv-cmd v0.1.0");
+    writeln("");
+    writeln("Options:");
 
-        foreach (it; helpInfo.options)
-        {
-            writefln("  %*s %*-s %s",
-                    longestShortOptLength,    it.optShort,
-                    longestLongOptLength + 2, it.optLong,
-                    it.help);
-        }
+    ulong longestShortOptLength = helpInfo.options.map!( opt => opt.optShort.length )
+        .reduce!((a, b) => max(a, b));
+    ulong longestLongOptLength  = helpInfo.options.map!( opt => opt.optLong.length  )
+        .reduce!((a, b) => max(a, b));
 
-        return 0;
+    foreach (it; helpInfo.options) {
+        writefln("  %*s %*-s %s",
+                longestShortOptLength,    it.optShort,
+                longestLongOptLength + 2, it.optLong,
+                it.help);
     }
+}
 
-    string message;
-    if (!options.validate(message))
-    {
-        writeln(message);
-        return 1;
-    }
+// Example hosts entry: 172.24.0.1  priv-dark-desktop  priv-dark-local
+string getEnvName() {
+    ulong prefixLength = ("priv-" ~ username ~ "-").length;
 
-    auto redis = new Redis("priv-dark-novaember", 6379);
-    auto commands = redis.send("SMEMBERS", "commands");
+    return readText("/etc/hosts")
+        .splitLines()                                       // Get array of host entries
+        .filter!( line => line != "" && line[0] != '#'    ) // Filter away comments and empty lines
+        .map!(    line => line.split()                    ) // Split lines into arrays of entries
+        .find!(   line => line.canFind("priv-dark-local") ) // Find line containing "priv-user-local" entry
+        .array[0].array[1]                                  // Get the hostname for the first matching entry
+        .drop(prefixLength);                                // Get hostname
+}
+
+void addBookmark(string command) {
+    redisMaster.send("SADD", "commands", command);
+}
+
+void removeBookmark(string command) {
+    redisMaster.send("SREM", "commands", command);
+    redisMaster.send("SREM", "commands:" ~ envName, command);
+}
+
+void addLocalBookmark(string command) {
+    redisMaster.send("SADD", "commands:" ~ envName, command);
+}
+
+void showCommandMenu() {
+    auto commands = redisLocal.send("SUNION", "commands", "commands:" ~ envName).values;
+    commands.randomShuffle();
 
     string[] dmenuCmd = [ "dmenu", "-p", "Run", "-nf", "#5433d6", "-sb", "#221556" ];
     auto dmenu = pipeProcess(dmenuCmd, Redirect.stdin | Redirect.stdout);
@@ -73,8 +88,60 @@ int main(string[] args)
     dmenu.stdin.flush();
     dmenu.stdin.close();
 
-    string cmd = dmenu.stdout.readln();
-    executeShell(cmd);
+    foreach (command; dmenu.stdout.byLine().map!(to!string)) {
+        if (command[0] == '-') {
+            long spaceIndex = command.indexOf(' ');
+            string dashCmd   = command[0..(spaceIndex)];
+            string cmdTarget = command.drop(spaceIndex + 1);
+
+            switch (dashCmd) {
+                case "--add":
+                case "-a":  addBookmark(cmdTarget);      break;
+
+                case "--remove":
+                case "-r":  removeBookmark(cmdTarget);   break;
+
+                case "--add-local":
+                case "-al": addLocalBookmark(cmdTarget); break;
+
+                default: break;
+            }
+
+            showCommandMenu();
+        } else {
+            executeShell(command);
+        }
+    }
+
+    wait(dmenu.pid);
+}
+
+int main(string[] args) {
+    helpInfo = getopt(args,
+            std.getopt.config.caseSensitive,
+            "a|add",    "Bookmark a command", &options.add,
+            "al|add",   "Bookmark a command locally", &options.add,
+            "r|remove", "Remove a bookmark",  &options.remove);
+
+    if (helpInfo.helpWanted) {
+        usage();
+        return 0;
+    }
+
+    string message;
+    if (!options.validate(message)) {
+        writeln(message);
+        usage();
+        return 1;
+    }
+
+    username = environment["USER"];
+    envName = getEnvName();
+
+    redisMaster = new Redis("priv-dark-master", 6379);
+    redisLocal = new Redis("priv-dark-local", 6379);
+
+    showCommandMenu();
 
     return 0;
 }
